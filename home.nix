82cdaebca40294b2d8c3fd9396ed0c5120eec5da
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-03-17 08:53:37 +0100

    fzf: use bat for preview in file widget

--- home.nix
+++ home.nix
@@ -99,7 +99,7 @@
     enable = true;
     changeDirWidgetCommand = "fd --type d";
     fileWidgetCommand = "fd --type f";
-    fileWidgetOptions = [ "--preview 'head {}'" ];
+    fileWidgetOptions = [ "--preview 'bat -n --color=always --paging=never -r :$FZF_PREVIEW_LINES {}'" ];
     defaultOptions = [ "--height 40%" "--min-height 10" ];
   };
 
