f632e9c25fb6ec0fc0f0d5a688805528af699f7b
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-04-04 23:20:20 +0200

    fzf: add preview to fman

--- fish/functions/fman.fish
+++ fish/functions/fman.fish
@@ -1,5 +1,13 @@
 function fman --description "Fuzzy-find a manual page"
-  set page (man -k . | fzf -q "'$argv[1]" --select-1 | sd '([^(]+) \(([^)]+)\).*' '$1.$2')
+  set page (
+    man -k . \
+    | fzf --query "'$argv[1]" --select-1 \
+      --preview "echo {} | sd '([^(]+) \(([^)]+)\).*' '\$1.\$2' | xargs man" \
+      --bind 'alt-up:preview-up,alt-down:preview-down' \
+      # --bind "tab:execute(echo '{}' | sd '([^(]+) \(([^)]+)\).*' '\$1.\$2' | xargs man)" \
+      --header 'alt-(up|down) to scroll preview' \
+    | sd '([^(]+) \(([^)]+)\).*' '$1.$2'
+  )
   if test -n "$page"
     man $page
   end
