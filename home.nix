44e8bf4ebf99d99ef22e234403c3301cb8a46c5c
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-04-04 22:32:01 +0200

    fzf: fix file picker

--- home.nix
+++ home.nix
@@ -105,7 +105,7 @@ in {
     enable = true;
     changeDirWidgetCommand = "fd --type d";
     fileWidgetCommand = "fd --type f";
-    fileWidgetOptions = [ "--preview 'bat -n --color=always --paging=never -r :$FZF_PREVIEW_LINES {}'" ];
+    fileWidgetOptions = [ "--preview 'bat -n --color=always --paging=never -r :\\$FZF_PREVIEW_LINES {}'" ];
     defaultOptions = [ "--height 40%" "--min-height 10" ];
   };
 
