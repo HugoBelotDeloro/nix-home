823556dbb4e070b094d315017cdc17e6e21e9c30
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-06-11 21:02:11 +0200

    syncthing: add device-specific default folder config

--- nixosModules/syncthing.nix
+++ nixosModules/syncthing.nix
@@ -3,7 +3,9 @@
 let
   devices = builtins.mapAttrs (deviceName: deviceConfig: { inherit (deviceConfig) id; }) cfg;
 
-  folders = builtins.mapAttrs (folderName: folderConfig: folderConfig // { devices = folderDevices folderName; }) cfg.${hostname}.folders;
+  folders = builtins.mapAttrs (folderName: folderConfig:
+    cfg.${hostname}.folderDefaultConfig // folderConfig // { devices = folderDevices folderName; })
+    cfg.${hostname}.folders;
 
   # Get the list of all devices declaring folder `folderName`
   folderDevices = folderName:
@@ -18,62 +20,50 @@ let
   cfg = {
       "framework-nixos" = {
         id = "XIQYERO-26LTWQK-WPBCUPF-AZCHTK3-3PYTFCI-BRTEOXG-6UV3GM6-AF23PA6";
+
+        folderDefaultConfig = {
+          watchDelay = 3600;
+          versioning = {
+            type = "simple";
+            params.keep = "5";
+          };
+          rescanInterval = 3600;
+        };
+
         folders = {
           "Perso" = {
             path = "/home/${username}/Documents/Perso";
             watchDelay = 60;
-            versioning = {
-              type = "simple";
-              params.keep = "5";
-            };
-            rescanInterval = 3600;
           };
-          "Shared" = {
-            path = "/home/${username}/Shared";
-            watchDelay = 3600;
-            versioning = {
-              type = "simple";
-              params.keep = "5";
-            };
-            rescanInterval = 3600;
-          };
+          "Shared".path = "/home/${username}/Shared";
         };
       };
 
       "tartelette" = {
         id = "P3XFVIV-JEJHOZ3-SLL767M-CXJT6RX-6WF6YSY-5ILHHZX-SFCOYP5-R2M3RAA";
+
+        folderDefaultConfig = {
+          watchDelay = 3600;
+          versioning = {
+            type = "staggered";
+            params = {
+              cleanInterval = "3600";
+              maxAge = "0";
+            };
+          };
+          rescanInterval = 3600;
+          type = "receiveonly";
+        };
+
         folders = {
-          "Perso" = {
-            path = "/var/lib/syncthing/Perso";
-            watchDelay = 3600;
-            versioning = {
-              type = "staggered";
-              params = {
-                cleanInterval = "3600";
-                maxAge = "0";
-              };
-            };
-            rescanInterval = 3600;
-            type = "receiveonly";
-          };
-          "Shared" = {
-            path = "/var/lib/syncthing/Shared";
-            watchDelay = 3600;
-            versioning = {
-              type = "staggered";
-              params = {
-                cleanInterval = "3600";
-                maxAge = "0";
-              };
-            };
-            rescanInterval = 3600;
-            type = "receiveonly";
-          };
+          "Perso".path = "/var/lib/syncthing/Perso";
+          "Shared".path = "/var/lib/syncthing/Shared";
         };
       };
 
       "asus" = {
         id = "ZCKMKCR-OLUKBI6-AHSYE4X-JBXGWPX-6B76U4Y-CLEUU3Q-OITQYHB-EF252AT";
+
         folders = {
           "Perso" = {};
           "Shared" = {};
