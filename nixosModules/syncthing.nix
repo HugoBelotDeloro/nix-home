9e4d8690c2d03ba1d2bd8cb039134a45dbd1c7c2
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-06-17 20:59:39 +0200

    syncthing: declare options

--- nixosModules/syncthing.nix
+++ nixosModules/syncthing.nix
@@ -1,36 +1,234 @@
 { config, pkgs, lib, hostname, username, ... }:
 
 let
-  devices = builtins.mapAttrs (deviceName: deviceConfig: { inherit (deviceConfig) id; }) cfg;
+  cfg = config.iridescent.services.syncthing;
 
-  folders = builtins.mapAttrs (folderName: folderConfig:
-    cfg.${hostname}.folderDefaultConfig // folderConfig // { devices = folderDevices folderName; })
-    cfg.${hostname}.folders;
+  folderConfigType = lib.types.attrsOf (lib.types.submodule ({ name, ... }: {
+    options = {
+      path = lib.mkOption {
+        type = lib.types.str // {
+          check = x: lib.types.str.check x && (lib.substring 0 1 x == "/" || lib.substring 0 2 x == "~/");
+          description = lib.types.str.description + " starting with / or ~/";
+        };
+        default = name;
+        description = lib.mdDoc ''
+          The path to the folder which should be shared.
+          Only absolute paths (starting with `/`) and paths relative to
+          the [user](#opt-services.syncthing.user)'s home directory
+          (starting with `~/`) are allowed.
+        '';
+      };
 
-  # Get the list of all devices declaring folder `folderName`
-  folderDevices = folderName:
-    builtins.filter (value: value != null)
-      (lib.mapAttrsToList (deviceName: hasFolder: if hasFolder then deviceName else null)
-        (builtins.mapAttrs
-          (deviceName: deviceConfig: (builtins.hasAttr folderName deviceConfig.folders))
-          cfg
-        )
-      );
+      versioning = lib.mkOption {
+        default = null;
+        description = lib.mdDoc ''
+          How to keep changed/deleted files with Syncthing.
+          There are 4 different types of versioning with different parameters.
+          See <https://docs.syncthing.net/users/versioning.html>.
+        '';
+        example = lib.literalExpression ''
+          [
+            {
+              versioning = {
+                type = "simple";
+                params.keep = "10";
+              };
+            }
+            {
+              versioning = {
+                type = "trashcan";
+                params.cleanoutDays = "1000";
+              };
+            }
+            {
+              versioning = {
+                type = "staggered";
+                fsPath = "/syncthing/backup";
+                params = {
+                  cleanInterval = "3600";
+                  maxAge = "31536000";
+                };
+              };
+            }
+            {
+              versioning = {
+                type = "external";
+                params.versionsPath = pkgs.writers.writeBash "backup" '''
+                  folderpath="$1"
+                  filepath="$2"
+                  rm -rf "$folderpath/$filepath"
+                ''';
+              };
+            }
+          ]
+        '';
+        type = with lib.types; nullOr (submodule {
+          options = {
+            type = lib.mkOption {
+              type = enum [ "external" "simple" "staggered" "trashcan" ];
+              description = lib.mdDoc ''
+                The type of versioning.
+                See <https://docs.syncthing.net/users/versioning.html>.
+              '';
+            };
+            fsPath = lib.mkOption {
+              default = "";
+              type = either str path;
+              description = lib.mdDoc ''
+                Path to the versioning folder.
+                See <https://docs.syncthing.net/users/versioning.html>.
+              '';
+            };
+            params = lib.mkOption {
+              type = attrsOf (either str path);
+              description = lib.mdDoc ''
+                The parameters for versioning. Structure depends on
+                [versioning.type](#opt-services.syncthing.folders._name_.versioning.type).
+                See <https://docs.syncthing.net/users/versioning.html>.
+              '';
+            };
+          };
+        });
+      };
 
-  cfg = {
+      rescanInterval = lib.mkOption {
+        type = lib.types.int;
+        default = 3600;
+        description = lib.mdDoc ''
+          How often the folder should be rescanned for changes.
+        '';
+      };
+
+      type = lib.mkOption {
+        type = lib.types.enum [ "sendreceive" "sendonly" "receiveonly" "receiveencrypted" ];
+        default = "sendreceive";
+        description = lib.mdDoc ''
+          Whether to only send changes for this folder, only receive them
+          or both. `receiveencrypted` can be used for untrusted devices. See
+          <https://docs.syncthing.net/users/untrusted.html> for reference.
+        '';
+      };
+
+      watchDelay = lib.mkOption {
+        type = lib.types.int;
+        default = 10;
+        description = lib.mdDoc ''
+          The delay (in seconds) after an inotify event is triggered.
+        '';
+      };
+    };
+  }));
+
+in
+{
+  options.iridescent.services.syncthing = {
+
+    enable = lib.mkEnableOption "syncthing custom module";
+
+    devices = lib.mkOption {
+      default = {};
+      description = ''
+      The configuration of the devices and folders for each machine.
+      '';
+      type = lib.types.attrsOf (lib.types.submodule ({ deviceName, ... }: {
+        options = {
+          name = lib.mkOption {
+            type = lib.types.str;
+            default = deviceName;
+            description = "The name of the device";
+          };
+
+          id = lib.mkOption {
+            type = lib.types.str;
+            description = "The device's ID.";
+          };
+
+          folders = lib.mkOption {
+            type = folderConfigType;
+            default = {};
+            description = "The configuration of each folder that should be shared with that machine";
+          };
+        };
+      }));
+    };
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "syncthing";
+      description = "The user to run Syncthing as.";
+    };
+
+    remoteAccess = lib.mkEnableOption "remote access to the control panel";
+  };
+
+  config.services.syncthing = lib.mkIf cfg.enable (let
+
+    # Get the list of all devices declaring folder `folderName`
+    folderDevices = folderName:
+      builtins.filter (value: value != null)
+        (lib.mapAttrsToList (deviceName: hasFolder: if hasFolder then deviceName else null)
+          (builtins.mapAttrs
+            (deviceName: deviceConfig: (builtins.hasAttr folderName deviceConfig.folders))
+            cfg.devices
+          )
+        );
+
+  in {
+    enable = true;
+    openDefaultPorts = true;
+
+    dataDir = lib.mkIf (cfg.user != "syncthing") "/home/${cfg.user}";
+
+    user = cfg.user;
+    group = lib.mkIf (cfg.user != "syncthing") "users";
+
+    guiAddress = if cfg.remoteAccess then "0.0.0.0:8384" else "localhost:8384";
+
+    devices = builtins.mapAttrs (deviceName: deviceConfig: { inherit (deviceConfig) id; }) cfg.devices;
+    folders = builtins.mapAttrs
+      (folderName: folderConfig: folderConfig // { devices = folderDevices folderName; } )
+      cfg.devices.${hostname}.folders;
+
+    overrideDevices = true;
+    overrideFolders = true;
+
+    extraOptions = {
+      gui = {
+        value = {
+          user = lib.mkIf cfg.remoteAccess "hugobd";
+          password = lib.mkIf cfg.remoteAccess "$2a$10$ZKhH1.a..GHsSeP51T3a.eC9zrgF0NnfgqgyXwiqs8yq.N05BLjbS";
+        };
+        tls = true;
+      };
+      options = {
+        announceLANAddresses = false; # Only use local network
+        relaysEnabled = false; # No relays
+        urAccepted = -1; # No Usage Reporting
+      };
+    };
+  });
+
+  config.iridescent.services.syncthing = {
+    enable = true;
+
+    remoteAccess = false;
+
+    user = username;
+
+    devices = {
       "framework-nixos" = {
         id = "XIQYERO-26LTWQK-WPBCUPF-AZCHTK3-3PYTFCI-BRTEOXG-6UV3GM6-AF23PA6";
 
-        folderDefaultConfig = {
-          watchDelay = 3600;
-          versioning = {
-            type = "simple";
-            params.keep = "5";
+        folders = let
+          folderDefaultConfig = {
+            watchDelay = 3600;
+            versioning = {
+              type = "simple";
+              params.keep = "5";
+            };
+            rescanInterval = 3600;
           };
-          rescanInterval = 3600;
-        };
-
-        folders = {
+        in builtins.mapAttrs (_deviceName: deviceConfig: folderDefaultConfig // deviceConfig) {
           "Perso" = {
             path = "/home/${username}/Documents/Perso";
             watchDelay = 60;
@@ -42,20 +240,20 @@ let
       "tartelette" = {
         id = "P3XFVIV-JEJHOZ3-SLL767M-CXJT6RX-6WF6YSY-5ILHHZX-SFCOYP5-R2M3RAA";
 
-        folderDefaultConfig = {
-          watchDelay = 3600;
-          versioning = {
-            type = "staggered";
-            params = {
-              cleanInterval = "3600";
-              maxAge = "0";
-            };
+        folders = let
+          folderDefaultConfig = {
+              watchDelay = 3600;
+              versioning = {
+                type = "staggered";
+                params = {
+                  cleanInterval = "3600";
+                  maxAge = "0";
+                };
+              };
+              rescanInterval = 3600;
+              type = "receiveonly";
           };
-          rescanInterval = 3600;
-          type = "receiveonly";
-        };
-
-        folders = {
+        in builtins.mapAttrs (_deviceName: deviceConfig: folderDefaultConfig // deviceConfig) {
           "Perso".path = "/var/lib/syncthing/Perso";
           "Shared".path = "/var/lib/syncthing/Shared";
         };
@@ -68,37 +266,6 @@ let
           "Perso" = {};
           "Shared" = {};
         };
-      };
-
-    };
-in
-{
-  services.syncthing = {
-    enable = true;
-    dataDir = "/home/${username}/";
-    openDefaultPorts = true;
-    configDir = "/home/${username}/.config/syncthing";
-    user = username;
-    group = "users";
-    guiAddress = "localhost:8384";
-
-    inherit devices folders;
-
-    overrideDevices = true;
-    overrideFolders = true;
-
-    extraOptions = {
-      gui = {
-        value = {
-          user = "hugobd";
-          password = "$2a$10$ZKhH1.a..GHsSeP51T3a.eC9zrgF0NnfgqgyXwiqs8yq.N05BLjbS";
-        };
-        tls = true;
-      };
-      options = {
-        announceLANAddresses = false; # Only use local network
-        relaysEnabled = false; # No relays
-        urAccepted = -1; # No Usage Reporting
       };
     };
   };
