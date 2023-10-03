ed94bb3127ccce60b84e688eaa37f22ba73d60ec
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-10-03 18:52:14 +0200

    tartelette/sd: fix modules missing

--- tartelette/default.nix
+++ tartelette/default.nix
@@ -4,32 +4,35 @@ let
   system = "aarch64-linux";
   hostname = "tartelette";
 
+  modules = [
+    ./system
+    flake-inputs.home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.${username} = ./home.nix;
+      home-manager.extraSpecialArgs = { inherit username flake-inputs; };
+    }
+  ];
+
+  specialArgs = { inherit username hostname flake-inputs; };
+
   nixosConfigurationParameters = {
-    inherit system;
-
-    modules = [
-      ./system
-      flake-inputs.home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.${username} = ./home.nix;
-        home-manager.extraSpecialArgs = { inherit username flake-inputs; };
-      }
-    ];
-
-    specialArgs = { inherit username hostname flake-inputs; };
+    inherit system modules specialArgs;
   };
 
 in {
   nixosConfiguration = flake-inputs.nixpkgs.lib.nixosSystem nixosConfigurationParameters;
 
-  nixosSDImage = flake-inputs.nixos-generators.nixosGenerate (nixosConfigurationParameters // {
+  nixosSDImage = flake-inputs.nixos-generators.nixosGenerate {
+    inherit system specialArgs;
+
     format = "sd-aarch64";
-    modules = [{
+
+    modules = modules ++ [{
       # At time of writing, the zfs-kernel package is broken which prevents building.
       # Somehow removing compression avoids the need for zfs.
       sdImage.compressImage = false;
     }];
-  });
+  };
 }
