1390fbb072c8ab7fbfeb829eeb0394a0fd7c6b0d
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-12-01 12:27:42 +0100

    nomad: disable vfio

--- nomad/system/vfio.nix
+++ nomad/system/vfio.nix
@@ -3,6 +3,6 @@
 {
   imports = [ flake-inputs.self.nixosModules.virtualisation.vfio ];
 
-  virtualisation.vfio.enable = true;
+  virtualisation.vfio.enable = false;
   virtualisation.vfio.devices = [ "1002:73df" "1002:ab28" ];
 }
