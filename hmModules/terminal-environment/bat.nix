36f4cbfb6988ae090f5582465153e2d6ff3653a8
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-12-02 21:15:45 +0100

    home: fix bat man pager

--- hmModules/terminal-environment/bat.nix
+++ hmModules/terminal-environment/bat.nix
@@ -1,6 +1,7 @@
 {
   programs.bat.enable = true;
 
+  home.sessionVariables.MANROFFOPT = "-c";
   home.sessionVariables.MANPAGER =
-    "sh -c 'col -bx | bat -l man -p --paging always'";
+    "sh -c 'col -bx | bat -l man -p'";
 }
